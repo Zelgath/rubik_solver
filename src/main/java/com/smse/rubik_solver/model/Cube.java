520f8095bef7e7f822cca9d9305f0ff4e44c7b97
LipaJunior <kamil.lipinski2004@gmail.com>
2025-02-27 18:33:27 +0100

    Improved the code and fixed some logical errors. Common logic for the moves is not yet done.

--- src/main/java/com/smse/rubik_solver/model/Cube.java
+++ src/main/java/com/smse/rubik_solver/model/Cube.java
@@ -1,34 +1,61 @@
 package com.smse.rubik_solver.model;
 
 import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+enum Color {
+    W, R, B, G, O, Y;
+}
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Component
 public class Cube {
-    static char[][] W = { { 'W', 'W', 'W' }, { 'W', 'W', 'W' }, { 'W', 'W', 'W' } };
-    static char[][] R = { { 'R', 'R', 'R' }, { 'R', 'R', 'R' }, { 'R', 'R', 'R' } };
-    static char[][] B = { { 'B', 'B', 'B' }, { 'B', 'B', 'B' }, { 'B', 'B', 'B' } };
-    static char[][] G = { { 'G', 'G', 'G' }, { 'G', 'G', 'G' }, { 'G', 'G', 'G' } };
-    static char[][] O = { { 'O', 'O', 'O' }, { 'O', 'O', 'O' }, { 'O', 'O', 'O' } };
-    static char[][] Y = { { 'Y', 'Y', 'Y' }, { 'Y', 'Y', 'Y' }, { 'Y', 'Y', 'Y' } };
+    private Color[][] W = {
+            { Color.W, Color.W, Color.W },
+            { Color.W, Color.W, Color.W },
+            { Color.W, Color.W, Color.W }
+    };
 
-    public Cube(char[][] W, char[][] R, char[][] B, char[][] G, char[][] O, char[][] Y) {
-        this.W = W;
-        this.R = R;
-        this.B = B;
-        this.G = G;
-        this.O = O;
-        this.Y = Y;
-    }
+    private Color[][] R = {
+            { Color.R, Color.R, Color.R },
+            { Color.R, Color.R, Color.R },
+            { Color.R, Color.R, Color.R }
+    };
 
-    public static void main(String[] args) {
-        Cube cube = new Cube(W, R, B, G, O, Y);
-        cube.printCube();
-        // cube.makeMovesFromList(cube.solveCrossOnLastLayer());
+    private Color[][] B = {
+            { Color.B, Color.B, Color.B },
+            { Color.B, Color.B, Color.B },
+            { Color.B, Color.B, Color.B }
+    };
 
-    }
+    private Color[][] G = {
+            { Color.G, Color.G, Color.G },
+            { Color.G, Color.G, Color.G },
+            { Color.G, Color.G, Color.G }
+    };
+
+    private Color[][] O = {
+            { Color.O, Color.O, Color.O },
+            { Color.O, Color.O, Color.O },
+            { Color.O, Color.O, Color.O }
+    };
+
+    private Color[][] Y = {
+            { Color.Y, Color.Y, Color.Y },
+            { Color.Y, Color.Y, Color.Y },
+            { Color.Y, Color.Y, Color.Y }
+    };
 
     private void printCube() {
         System.out.println("Up");
@@ -46,14 +73,14 @@ public class Cube {
     }
 
     private boolean isCubeCompleted() {
-        if (isFaceComplted(W) && isFaceComplted(R) && isFaceComplted(B) && isFaceComplted(G) && isFaceComplted(O)
-                && isFaceComplted(Y)) {
+        if (isFaceCompleted(W) && isFaceCompleted(R) && isFaceCompleted(B) && isFaceCompleted(G) && isFaceCompleted(O)
+                && isFaceCompleted(Y)) {
             return true;
         }
         return false;
     }
 
-    private static boolean isFaceComplted(char[][] face) {
+    private static boolean isFaceCompleted(Color[][] face) {
         for (int i = 0; i < face.length; i++) {
             for (int j = 0; j < face.length; j++) {
                 if (face[i][j] != face[1][1]) {
@@ -64,7 +91,7 @@ public class Cube {
         return true;
     }
 
-    private static void printFace(char[][] face) {
+    private static void printFace(Color[][] face) {
         for (int i = 0; i < face.length; i++) {
             for (int j = 0; j < face.length; j++) {
                 System.out.print(face[i][j] + " ");
@@ -73,8 +100,8 @@ public class Cube {
         }
     }
 
-    private static void rotateClockWise(char[][] face) {
-        char[][] temp = new char[3][3];
+    private static void rotateClockWise(Color[][] face) {
+        Color[][] temp = new Color[3][3];
         for (int i = 0; i < face.length; i++) {
             for (int j = 0; j < face.length; j++) {
                 temp[j][2 - i] = face[i][j];
@@ -88,8 +115,8 @@ public class Cube {
 
     }
 
-    private static void rotateAntiClockWise(char[][] face) {
-        char[][] temp = new char[3][3];
+    private static void rotateAntiClockWise(Color[][] face) {
+        Color[][] temp = new Color[3][3];
         for (int i = 0; i < face.length; i++) {
             for (int j = 0; j < face.length; j++) {
                 temp[2 - j][i] = face[i][j];
@@ -104,158 +131,248 @@ public class Cube {
 
     private void moveR() {
         rotateClockWise(B);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
+
         for (int i = 0; i < 3; i++) {
             temp[i] = W[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             W[i][2] = R[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             R[i][2] = Y[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             Y[i][2] = O[2 - i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             O[2 - i][0] = temp[i];
         }
-        printCube();
     }
 
     private void moveRprim() {
         rotateAntiClockWise(B);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
+
         for (int i = 0; i < 3; i++) {
             temp[i] = W[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             W[i][2] = O[2 - i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             O[2 - i][0] = Y[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             Y[i][2] = R[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             R[i][2] = temp[i];
         }
-        printCube();
     }
 
     private void moveL() {
         rotateClockWise(G);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
+
         for (int i = 0; i < 3; i++) {
             temp[i] = W[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             W[i][0] = O[2 - i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             O[2 - i][2] = Y[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             Y[i][0] = R[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             R[i][0] = temp[i];
         }
-        printCube();
     }
 
     private void moveLprim() {
         rotateAntiClockWise(G);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
+
         for (int i = 0; i < 3; i++) {
             temp[i] = W[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             W[i][0] = R[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             R[i][0] = Y[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             Y[i][0] = O[2 - i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             O[2 - i][2] = temp[i];
         }
-        printCube();
     }
 
     private void moveU() {
         rotateClockWise(W);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
+
         for (int i = 0; i < 3; i++) {
             temp[i] = R[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             R[0][i] = B[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             B[0][i] = O[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             O[0][i] = G[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             G[0][i] = temp[i];
         }
-        printCube();
     }
 
     private void moveUprim() {
         rotateAntiClockWise(W);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
+
         for (int i = 0; i < 3; i++) {
             temp[i] = R[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             R[0][i] = G[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             G[0][i] = O[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             O[0][i] = B[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             B[0][i] = temp[i];
         }
-        printCube();
     }
 
     private void moveF() {
         rotateClockWise(R);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
         for (int i = 0; i < 3; i++) {
             temp[i] = W[2][i];
-            W[2][i] = G[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
+            W[2][i] = G[2 - i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             G[i][2] = Y[0][i];
-            Y[0][i] = B[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
+            Y[0][i] = B[2 - i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             B[i][0] = temp[i];
         }
-        printCube();
     }
 
     private void moveFprim() {
         rotateAntiClockWise(R);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
         for (int i = 0; i < 3; i++) {
             temp[i] = W[2][i];
-            W[2][i] = B[i][0];
-            B[i][0] = Y[0][i];
-            Y[0][i] = G[i][2];
-            G[i][2] = temp[i];
         }
-        printCube();
+        for (int i = 0; i < 3; i++) {
+            W[2][i] = B[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
+            B[i][0] = Y[0][2 - i];
+        }
+        for (int i = 0; i < 3; i++) {
+            Y[0][i] = G[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
+            G[i][2] = temp[2 - i];
+        }
     }
 
     private void moveD() {
         rotateClockWise(Y);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
         for (int i = 0; i < 3; i++) {
             temp[i] = R[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             R[2][i] = G[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             G[2][i] = O[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             O[2][i] = B[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             B[2][i] = temp[i];
         }
-        printCube();
     }
 
     private void moveDprim() {
         rotateAntiClockWise(Y);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
         for (int i = 0; i < 3; i++) {
             temp[i] = R[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             R[2][i] = B[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             B[2][i] = O[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             O[2][i] = G[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             G[2][i] = temp[i];
         }
-        printCube();
     }
 
     private void moveB() {
         rotateClockWise(O);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
         for (int i = 0; i < 3; i++) {
             temp[i] = W[0][i];
+        }
+        for (int i = 0; i < 3; i++) {
             W[0][i] = B[i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             B[i][2] = Y[2][2 - i];
+        }
+        for (int i = 0; i < 3; i++) {
             Y[2][i] = G[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             G[2 - i][0] = temp[i];
         }
-        printCube();
     }
 
     private void moveBprim() {
         rotateAntiClockWise(O);
-        char[] temp = new char[3];
+        Color[] temp = new Color[3];
         for (int i = 0; i < 3; i++) {
             temp[i] = W[0][i];
-            W[0][i] = G[i][0];
+        }
+        for (int i = 0; i < 3; i++) {
+            W[0][i] = G[2 - i][0];
+        }
+        for (int i = 0; i < 3; i++) {
             G[i][0] = Y[2][i];
+        }
+        for (int i = 0; i < 3; i++) {
             Y[2][i] = B[2 - i][2];
+        }
+        for (int i = 0; i < 3; i++) {
             B[i][2] = temp[i];
         }
-        printCube();
     }
 
     private void sexyMove() {
@@ -266,71 +383,82 @@ public class Cube {
     }
 
     private String[] solveCrossOnLastLayer() {
-        char center = Y[1][1];
-        char upper = Y[0][1];
-        char left = Y[1][0];
-        char right = Y[1][2];
-        char down = Y[2][1];
-        if (upper != center && left != center && right != center && down != center) {
-            return new String[] { "F", "L", "D", "L'", "D'", "F'", "B", "R", "D", "R'", "D'", "R", "D", "R'", "D'",
-                    "B'" };
-        } else if (upper == center && left == center) {
-            return new String[] { "B", "R", "D", "R'", "D'", "R", "D", "R'", "D'", "B'" };
-        } else if (upper == center && right == center) {
-            return new String[] { "L", "B", "U", "B'", "U'", "B", "U", "B'", "U'", "L'" };
-        } else if (down == center && left == center) {
-            return new String[] { "R", "F", "D", "F'", "D'", "F", "D", "F'", "D'", "R'" };
-        } else if (down == center && right == center) {
-            return new String[] { "F", "L", "D", "L'", "D'", "L", "D", "L'", "D'", "F'" };
-        } else if (down == center && upper == center) {
-            return new String[] { "L", "B", "D", "B'", "D'", "L'" };
-        } else if (left == center && right == center) {
-            return new String[] { "F", "L", "D", "L'", "D'", "F'" };
-        } else {
-            return null;
-        }
+        Color center = Y[1][1];
+        String state = "" + (Y[0][1] == center ? "U" : "") + (Y[1][0] == center ? "L" : "") +
+                (Y[1][2] == center ? "R" : "") + (Y[2][1] == center ? "D" : "");
 
+        switch (state) {
+            case "":
+                return new String[] { "F", "L", "D", "L'", "D'", "F'", "B", "R", "D", "R'", "D'", "R", "D", "R'", "D'",
+                        "B'" };
+            case "UL":
+                return new String[] { "B", "R", "D", "R'", "D'", "R", "D", "R'", "D'", "B'" };
+            case "UR":
+                return new String[] { "L", "B", "U", "B'", "U'", "B", "U", "B'", "U'", "L'" };
+            case "LD":
+                return new String[] { "R", "F", "D", "F'", "D'", "F", "D", "F'", "D'", "R'" };
+            case "RD":
+                return new String[] { "F", "L", "D", "L'", "D'", "L", "D", "L'", "D'", "F'" };
+            case "UD":
+                return new String[] { "L", "B", "D", "B'", "D'", "L'" };
+            case "LR":
+                return new String[] { "F", "L", "D", "L'", "D'", "F'" };
+            default:
+                return null;
+        }
     }
 
     private String[] solveLastLayerPart2() {
         List<String> moves = new ArrayList<>();
         int count = howManyStickersInPlaceForPart2();
+        int c = 0;
 
         while (count < 2) {
             moveD();
+            c++;
             moves.add("D");
             count = howManyStickersInPlaceForPart2();
         }
 
-        char frontSticker = R[2][1];
-        char frontCenter = R[1][1];
-        char rightSticker = B[2][1];
-        char rightCenter = B[1][1];
-        char leftSticker = G[2][1];
-        char leftCenter = G[1][1];
-        char backSticker = O[2][1];
-        char backCenter = O[1][1];
+        Color frontSticker = R[2][1];
+        Color frontCenter = R[1][1];
+        Color rightSticker = B[2][1];
+        Color rightCenter = B[1][1];
+        Color leftSticker = G[2][1];
+        Color leftCenter = G[1][1];
+        Color backSticker = O[2][1];
+        Color backCenter = O[1][1];
+        String state = "" + (frontSticker == frontCenter ? "F" : "") + (rightSticker == rightCenter ? "R" : "") +
+                (leftSticker == leftCenter ? "L" : "") + (backSticker == backCenter ? "B" : "");
+
+        for (int i = 0; i < c; i++) {
+            moveDprim();
+        }
 
         String[] additionalMoves;
-        if (frontSticker == frontCenter && rightSticker == rightCenter && leftSticker == leftCenter
-                && backSticker == backCenter) {
-            return moves.toArray(new String[0]);
-        } else if (rightSticker == rightCenter && backSticker == backCenter) {
-            additionalMoves = new String[] { "D'", "B", "D'", "D'", "B'", "D'", "B", "D'", "B'" };
-        } else if (rightSticker == rightCenter && frontSticker == frontCenter) {
-            additionalMoves = new String[] { "D'", "R", "D'", "D'", "R'", "D'", "R", "D'", "R'" };
-        } else if (leftSticker == leftCenter && backSticker == backCenter) {
-            additionalMoves = new String[] { "D'", "L", "D'", "D'", "L'", "D'", "L", "D'", "L'" };
-        } else if (leftSticker == leftCenter && frontSticker == frontCenter) {
-            additionalMoves = new String[] { "D'", "F", "D'", "D'", "F'", "D'", "F", "D'", "F'" };
-        } else if (rightSticker == rightCenter && leftSticker == leftCenter) {
-            additionalMoves = new String[] { "D'", "L", "D'", "D'", "L'", "D'", "L", "D'", "L'", "B", "D'",
-                    "D'", "B'", "D'", "B", "D'", "B'" };
-        } else if (frontSticker == frontCenter && backSticker == backCenter) {
-            additionalMoves = new String[] { "D'", "B", "D'", "D'", "B'", "D'", "B", "D'", "B'", "R", "D'",
-                    "D'", "R'", "D'", "R", "D'", "R'" };
-        } else {
-            additionalMoves = new String[] {};
+        switch (state) {
+            case ("RB"):
+                additionalMoves = new String[] { "D'", "B", "D'", "D'", "B'", "D'", "B", "D'", "B'" };
+                break;
+            case ("FR"):
+                additionalMoves = new String[] { "D'", "R", "D'", "D'", "R'", "D'", "R", "D'", "R'" };
+                break;
+            case ("LB"):
+                additionalMoves = new String[] { "D'", "L", "D'", "D'", "L'", "D'", "L", "D'", "L'" };
+                break;
+            case ("FL"):
+                additionalMoves = new String[] { "D'", "F", "D'", "D'", "F'", "D'", "F", "D'", "F'" };
+                break;
+            case ("RL"):
+                additionalMoves = new String[] { "D'", "L", "D'", "D'", "L'", "D'", "L", "D'", "L'", "B", "D'",
+                        "D'", "B'", "D'", "B", "D'", "B'" };
+                break;
+            case ("FB"):
+                additionalMoves = new String[] { "D'", "B", "D'", "D'", "B'", "D'", "B", "D'", "B'", "R", "D'",
+                        "D'", "R'", "D'", "R", "D'", "R'" };
+                break;
+            default:
+                additionalMoves = new String[] {};
         }
 
         moves.addAll(Arrays.asList(additionalMoves));
@@ -340,14 +468,14 @@ public class Cube {
 
     private int howManyStickersInPlaceForPart2() {
         int count = 0;
-        char frontSticker = R[2][1];
-        char frontCenter = R[1][1];
-        char rightSticker = B[2][1];
-        char rightCenter = B[1][1];
-        char leftSticker = G[2][1];
-        char leftCenter = G[1][1];
-        char backSticker = O[2][1];
-        char backCenter = O[1][1];
+        Color frontSticker = R[2][1];
+        Color frontCenter = R[1][1];
+        Color rightSticker = B[2][1];
+        Color rightCenter = B[1][1];
+        Color leftSticker = G[2][1];
+        Color leftCenter = G[1][1];
+        Color backSticker = O[2][1];
+        Color backCenter = O[1][1];
         if (frontSticker == frontCenter) {
             count++;
         }
@@ -364,6 +492,9 @@ public class Cube {
     }
 
     private void makeMovesFromList(String[] moves) {
+        if (moves.length == 0) {
+            return;
+        }
         for (int i = 0; i < moves.length; i++) {
             switch (moves[i]) {
                 case "R":
