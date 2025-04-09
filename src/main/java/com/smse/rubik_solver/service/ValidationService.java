eb85141bead2026d0ff2628d79f520835e211ca8
LipaJunior <kamil.lipinski2004@gmail.com>
2025-04-09 15:15:24 +0200

    center pieces validation in ValidationService

--- src/main/java/com/smse/rubik_solver/service/ValidationService.java
+++ src/main/java/com/smse/rubik_solver/service/ValidationService.java
@@ -1,12 +1,15 @@
 package com.smse.rubik_solver.service;
 
 import org.springframework.stereotype.Service;
+
+import com.smse.rubik_solver.model.Color;
 import com.smse.rubik_solver.model.Cube;
 
 @Service
 public class ValidationService {
     public boolean isCubeValid(Cube cube) {
-        // TODO write logic for validation
-        return true;
+        return (cube.getFront().get(1).get(1) == Color.R && cube.getUp().get(1).get(1) == Color.W
+                && cube.getRight().get(1).get(1) == Color.B && cube.getLeft().get(1).get(1) == Color.G
+                && cube.getDown().get(1).get(1) == Color.Y && cube.getBack().get(1).get(1) == Color.O);
     }
 }
