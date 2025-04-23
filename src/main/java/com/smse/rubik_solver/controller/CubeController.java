4b4ae15e984837e083b0425e897b9935c8c080fa
LipaJunior <kamil.lipinski2004@gmail.com>
2025-04-23 22:57:15 +0200

    sessionID works correctly

--- src/main/java/com/smse/rubik_solver/controller/CubeController.java
+++ src/main/java/com/smse/rubik_solver/controller/CubeController.java
@@ -1,8 +1,6 @@
 package com.smse.rubik_solver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.smse.rubik_solver.dto.CubeMoveRequest;
 import com.smse.rubik_solver.dto.CubeScrambleRequest;
 import com.smse.rubik_solver.dto.InitResponseDto;
@@ -11,12 +9,8 @@ import com.smse.rubik_solver.model.UserSession;
 import com.smse.rubik_solver.service.CubeService;
 import com.smse.rubik_solver.service.SessionManager;
 import com.smse.rubik_solver.service.ValidationService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/cube")
@@ -27,56 +21,92 @@ public class CubeController {
     private final SessionManager sessionManager;
     private final ValidationService validationService;
 
-    @PostMapping("/init")
-    public ResponseEntity<InitResponseDto> initCube(@RequestBody Cube c) {
-        Cube cube = cubeService.initializeCube(c);
-        if (!validationService.isCubeValid(cube)) {
-            return ResponseEntity.badRequest().build();
+    private String getOrCreateSessionId(String sessionId) {
+        if (sessionId != null && sessionManager.getSession(sessionId) != null) {
+            return sessionId;
+        } else {
+            return sessionManager.createSession();
         }
+    }
 
-        String sessionId = sessionManager.createSession();
-        UserSession session = sessionManager.getSession(sessionId);
-        session.setCube(cube);
-
+    private ResponseEntity<InitResponseDto> buildResponse(Cube cube, String sessionId) {
         InitResponseDto response = InitResponseDto.builder()
                 .sessionId(sessionId)
                 .cube(cube)
                 .build();
-
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/move")
-    public ResponseEntity<Cube> applyMoves(@RequestBody CubeMoveRequest request) {
+    @PostMapping("/init")
+    public ResponseEntity<InitResponseDto> initCube(
+            @RequestBody Cube c,
+            @RequestParam(required = false) String sessionId) {
         try {
+            Cube cube = cubeService.initializeCube(c);
+            if (!validationService.isCubeValid(cube)) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            sessionId = getOrCreateSessionId(sessionId);
+            UserSession session = sessionManager.getSession(sessionId);
+            session.setCube(cube);
+
+            return buildResponse(cube, sessionId);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/move")
+    public ResponseEntity<InitResponseDto> applyMoves(
+            @RequestBody CubeMoveRequest request,
+            @RequestParam(required = false) String sessionId) {
+        try {
+            sessionId = getOrCreateSessionId(sessionId);
+            UserSession session = sessionManager.getSession(sessionId);
+
             Cube cube = cubeService.initializeCube(request.getCube());
+
             cubeService.applyMoves(cube, request.getMoves());
-            return ResponseEntity.ok(cube);
+            session.setCube(cube);
+
+            return buildResponse(cube, sessionId);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @PostMapping("/scramble")
-    public ResponseEntity<Cube> scramble(@RequestBody CubeScrambleRequest request) {
+    public ResponseEntity<InitResponseDto> scramble(
+            @RequestBody CubeScrambleRequest request,
+            @RequestParam(required = false) String sessionId) {
         try {
+            sessionId = getOrCreateSessionId(sessionId);
+            UserSession session = sessionManager.getSession(sessionId);
+
             Cube cube = cubeService.initializeCube(request.getCube());
             cubeService.getRandomScramble(cube, request.getN());
-            return ResponseEntity.ok(cube);
+            session.setCube(cube);
+
+            return buildResponse(cube, sessionId);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
-    @GetMapping("/random")
-    public ResponseEntity<Cube> random() {
+    @PostMapping("/random")
+    public ResponseEntity<InitResponseDto> random(@RequestParam(required = false) String sessionId) {
         try {
+            sessionId = getOrCreateSessionId(sessionId);
+            UserSession session = sessionManager.getSession(sessionId);
+
             Cube cube = cubeService.createSolvedCube();
             cubeService.getRandomScramble(cube, 20);
-            return ResponseEntity.ok(cube);
+            session.setCube(cube);
+
+            return buildResponse(cube, sessionId);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
-
 }
