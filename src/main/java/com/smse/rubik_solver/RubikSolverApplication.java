28fb7a4e057c1a76bbe9b8856d7b7a7b8276aacc
LipaJunior <kamil.lipinski2004@gmail.com>
2025-04-29 19:31:06 +0200

    Swagger UI link in terminal

--- src/main/java/com/smse/rubik_solver/RubikSolverApplication.java
+++ src/main/java/com/smse/rubik_solver/RubikSolverApplication.java
@@ -2,12 +2,21 @@ package com.smse.rubik_solver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RubikSolverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RubikSolverApplication.class, args);
+		var app = SpringApplication.run(RubikSolverApplication.class, args);
+		emitSwaggerLink(app);
+	}
+
+	private static void emitSwaggerLink(ConfigurableApplicationContext app) {
+		if (java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments().toString().contains("jdwp")) {
+			String port = app.getEnvironment().getProperty("server.port", "8080");
+			System.out.println("Swagger UI: http://localhost:" + port + "/swagger-ui/index.html");
+		}
 	}
 
 }
